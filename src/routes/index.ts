ebde08ce156186aeb16a76de0025723c76a223e7
Romina Rebolledo <rominarebolledo@gmail.com>
2022-04-27 21:48:47 +0200

    fix in addNewImage fuction

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ userRouter.post("/register", userController.registerUser);
 userRouter.post("/login", userController.signInWithEmailAndPassword); // need auth
 // userRouter.delete("/deleteuser", userController.deleteUser); // need auth
 userRouter.post("/passwordreset", userController.passwordReset)
-userRouter.post("/:id/image", userController.addImageAndDescription)
+userRouter.post("/:id/image", auth, userController.addImageAndDescription)
 // userRouter.put("/update", auth, userController.updateUser); // need auth
 
 
