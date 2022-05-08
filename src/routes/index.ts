4ba0abe8991cbc6b953335c2b6fdda46b61c51b4
Romina Rebolledo <rominarebolledo@gmail.com>
2022-05-08 22:23:47 +0200

    function add image fixed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,10 +9,9 @@ userRouter.post("/register", userController.registerUser);
 userRouter.post("/login", userController.signInWithEmailAndPassword); // need auth
 // userRouter.delete("/deleteuser", userController.deleteUser); // need auth
 userRouter.post("/passwordreset", userController.passwordReset)
-userRouter.post("/:id/image", auth, userController.addImageAndDescription)
+userRouter.post("/image", auth,  userController.addTitleAndDescription)
+// userRouter.post("/:id/nuevaimagen", auth,  userController.addNewImage)
+
 // userRouter.put("/update", auth, userController.updateUser); // need auth
-
-
-
 
 export default userRouter;
